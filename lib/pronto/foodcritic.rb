88477599936d8f470fa63ddb9285c8486b412136
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2016-03-14 22:54:07 +0200

    Update runner to expect parameters via initialize

--- lib/pronto/foodcritic.rb
+++ lib/pronto/foodcritic.rb
@@ -3,42 +3,31 @@ require 'foodcritic'
 
 module Pronto
   class FoodCritic < Runner
-    def initialize
-      @linter = ::FoodCritic::Linter.new
-    end
-
-    def run(patches, _)
-      return [] unless patches
-
-      ruby_patches = patches.select { |patch| patch.additions > 0 }
-        .select { |patch| ruby_file?(patch.new_file_full_path) }
-
-      inspect(ruby_patches)
-    end
-
-    def inspect(patches)
-      paths = chef_paths(patches)
+    def run
       return [] if paths[:cookbook_paths].none? && paths[:role_paths].none?
 
+      @linter = ::FoodCritic::Linter.new
       @linter.check(paths).warnings.flat_map do |warning|
-        patches.select { |patch| patch.new_file_full_path.to_s == warning.match[:filename] }
+        ruby_patches.select { |patch| patch.new_file_full_path.to_s == warning.match[:filename] }
           .flat_map(&:added_lines)
           .select { |line| line.new_lineno == warning.match[:line] }
           .flat_map { |line| new_message(warning, line) }
       end
     end
 
-    def chef_paths(patches)
-      paths = { cookbook_paths: [], role_paths: [] }
-      patches.each do |patch|
-        path = patch.new_file_full_path.to_s
-        if path.include?('cookbook')
-          paths[:cookbook_paths] << path
-        elsif path.include?('role')
-          paths[:role_paths] << path
+    def paths
+      @paths ||= begin
+        result = { cookbook_paths: [], role_paths: [] }
+        ruby_patches.each do |patch|
+          path = patch.new_file_full_path.to_s
+          if path.include?('cookbook')
+            result[:cookbook_paths] << path
+          elsif path.include?('role')
+            result[:role_paths] << path
+          end
         end
+        result
       end
-      paths
     end
 
     def new_message(warning, line)
