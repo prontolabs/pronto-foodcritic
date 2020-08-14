9d79815f4230432c5a497069eac580c4103f4e74
Vytautas Stankevičius <vytautas.stankevicius@vinted.com>
2020-08-14 09:55:37 +0300

    Add foodcritic several options support to .pronto.yml

--- lib/pronto/foodcritic.rb
+++ lib/pronto/foodcritic.rb
@@ -7,11 +7,21 @@ module Pronto
       return [] if paths[:cookbook_paths].none? && paths[:role_paths].none?
 
       @linter = ::FoodCritic::Linter.new
-      @linter.check(paths).warnings.flat_map do |warning|
+      @linter.check(options).warnings.flat_map do |warning|
         ruby_patches.select { |patch| patch.new_file_full_path.to_s == warning.match[:filename] }
           .flat_map(&:added_lines)
           .select { |line| line.new_lineno == warning.match[:line] }
           .flat_map { |line| new_message(warning, line) }
+      end
+    end
+
+    def options
+      @options ||= begin
+        result = {}.merge(paths)
+        result[:rule_file] = foodcritic_rule_file if foodcritic_rule_file
+        result[:search_gems] = foodcritic_search_gems if foodcritic_search_gems
+        result[:include_rules] = Array(foodcritic_include_rules)
+        result
       end
     end
 
@@ -20,6 +30,7 @@ module Pronto
         result = { cookbook_paths: [], role_paths: [] }
         ruby_patches.each do |patch|
           path = patch.new_file_full_path.to_s
+          next if foodcritic_exclude.any? { |r| File.fnmatch(r, path) }
           if path.include?('cookbook')
             result[:cookbook_paths] << path
           elsif path.include?('role')
@@ -35,5 +46,26 @@ module Pronto
       message = "#{warning.rule.code} - #{warning.rule.name}"
       Message.new(path, line, :warning, message, nil, self.class)
     end
+
+    def pronto_foodcritic_config
+      @pronto_foodcritic_config ||= Pronto::ConfigFile.new.to_h['foodcritic'] || {}
+    end
+
+    def foodcritic_search_gems
+      pronto_foodcritic_config.fetch('search_gems', nil)
+    end
+
+    def foodcritic_rule_file
+      pronto_foodcritic_config.fetch('rule_file', nil)
+    end
+
+    def foodcritic_include_rules
+      pronto_foodcritic_config.fetch('include_rules', [])
+    end
+
+    def foodcritic_exclude
+      Array(pronto_foodcritic_config.fetch('exclude', []))
+    end
+
   end
 end
